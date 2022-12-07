b9de8e642d93e82146d1883dfd2d8e5e98dd0f05
Vlad Negrea <negreamihneavlad@gmail.com>
2022-12-07 13:53:42 +0200

    Implement Snap Scout

---
+++ src/context/PhotoContext.js
@@ -0,0 +1,29 @@
+import React, { createContext, useState } from "react";
+import axios from "axios";
+export const PhotoContext = createContext();
+
+const PhotoContextProvider = (props) => {
+  const [images, setImages] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const runSearch = (query) => {
+    axios
+      .get(`http://localhost:3000/photos?query=${query}`)
+      .then((response) => {
+        setImages(response.data.photos.photo);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log(
+          "Encountered an error with fetching and parsing data",
+          error
+        );
+      });
+  };
+  return (
+    <PhotoContext.Provider value={{ images, loading, runSearch }}>
+      {props.children}
+    </PhotoContext.Provider>
+  );
+};
+
+export default PhotoContextProvider;
